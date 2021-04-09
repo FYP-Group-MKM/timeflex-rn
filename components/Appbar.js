8288a8965f9572c0f9335948267413a7f784dabf
Joe Lam <lamtingfaijoe@gmail.com>
2021-04-09 16:21:54 +0800

    Revert "appbar date string and today button"
    This reverts commit 9f7add458b896be55461d8d16e246d9fd922b706.

--- components/Appbar.js
+++ components/Appbar.js
@@ -1,19 +1,16 @@
 import format from 'date-fns/format';
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { State } from 'react-native-gesture-handler';
 import { Appbar as PaperAppbar } from 'react-native-paper';
-import { connect } from 'react-redux';
-import { setCurrentDate } from '../actions';
 
-const Appbar = ({ navigation, currentDate, setCurrentDate }) => {
-    const dateString = format(currentDate, 'MMM yyyy');
+const Appbar = ({ navigation }) => {
+    const dateString = format(new Date(), 'MMM yyyy');
 
     return (
         <PaperAppbar.Header style={styles.root}>
             <PaperAppbar.Action icon={'menu'} onPress={navigation.toggleDrawer} />
             <PaperAppbar.Content title={dateString} />
-            <PaperAppbar.Action icon={'calendar-today'} onPress={() => setCurrentDate(new Date())} />
+            <PaperAppbar.Action icon={'calendar-today'} onPress={() => { }} />
         </PaperAppbar.Header >
     );
 }
@@ -33,12 +30,4 @@ const styles = StyleSheet.create({
     }
 });
 
-const mapStateToProps = (state) => ({
-    currentDate: state.calendar.currentDate,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-    setCurrentDate: (date) => dispatch(setCurrentDate(date)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Appbar);
+export default Appbar;
