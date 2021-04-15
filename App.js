a7bc49c6920b044debcf991d594b00061e40ef4c
darrenleung1208hk <leungyatlong1208@gmail.com>
2021-04-16 01:05:18 +0800

    login screen

--- App.js
+++ App.js
@@ -8,11 +8,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { connect } from 'react-redux';
 import { fetchAppointments, setUser } from './actions';
 
-import * as Linking from 'expo-linking'
-import * as WebBrowser from 'expo-web-browser';
-
 import HomeScreen from './screens/HomeScreen';
 import LogoutScreen from './screens/LogoutScreen';
+import LoginScreen from './screens/LoginScreen';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -24,27 +22,6 @@ const App = (props) => {
             .then(user => props.setUser(JSON.parse(user)))
             .then(props.fetchAppointments());
     }, []);
-
-    const handleRedirect = async event => {
-        WebBrowser.dismissBrowser();
-    };
-
-    const handleOAuthLogin = async () => {
-        let redirectUrl = await Linking.getInitialURL();
-        Linking.addEventListener('url', handleRedirect);
-        try {
-            let authResult = await WebBrowser.openAuthSessionAsync(`https://timeflex-web.herokuapp.com/expo-auth/google`, redirectUrl);
-            const userURIComponent = authResult.url.replace('exp://exp.host/@darren1208/timeflex-rn/', '');
-            const userJSON = decodeURIComponent(userURIComponent);
-            await AsyncStorage.setItem('timeflexUser', userJSON)
-                .then(props.setUser(JSON.parse(userJSON)))
-                // .then(props.fetchAppointments())
-                .catch(error => console.log(error));
-        } catch (err) {
-            console.log('ERROR:', err);
-        }
-        Linking.removeEventListener('url', handleRedirect);
-    };
 
     const routes = ['week', '3days', 'day'].map(mode => {
         const renderScreen = ({ navigation }) => <HomeScreen navigation={navigation} mode={mode} />
@@ -64,9 +41,7 @@ const App = (props) => {
                 <Drawer.Screen name='Logout' component={LogoutScreen} />
             </Drawer.Navigator>
         </NavigationContainer>
-        : <SafeAreaView>
-            <Button title='login' onPress={handleOAuthLogin}>Login</Button>
-        </SafeAreaView >;
+        : <LoginScreen />;
 };
 
 const mapStateToProps = (state) => ({
